7cb486091f91ad9b5b2b44b6b058415d057ac737
mikaelhen <mikaelhen28@gmail.com>
2022-02-09 11:41:09 +0100

    maj

---
+++ php/div2.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>texte+bouton</title>
+</head>
+<body>
+    <div class="contenant">
+        <div class="text">
+            <div class="titre">
+            <h1>MARIAGE, BAPTEME, COMMUNION ...</h1>
+            </div>
+        <div class="textebook">
+            <h5>Nous réalisons des books pour tous évènements, des tarifs sont établis à la prestation.</h5>
+        </div>
+        </div>
+            <div class="bouton">                
+                <a href="https://fr.photographes.com/?gclid=EAIaIQobChMIiOe3wI_y9QIVhuFRCh2P5wyWEAAYASAAEgL_TvD_BwE" class="button"> cliquer </a>  
+                                        
+        </div>       
+    </div>
+    
+</body>
+</html>
